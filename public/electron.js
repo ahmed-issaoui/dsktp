0e6517cc28dd80b0240d36ae060e2f97ed464687
ahmed-issaoui <pro.ahmed.issaoui@gmail.com>
2022-11-09 00:11:21 +0100

    cookies + notification from puppeteer to desktop

--- public/electron.js
+++ public/electron.js
@@ -52,10 +52,14 @@ const createWindow = () => {
 
 };
 
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow);
+
+
+
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -101,7 +105,22 @@ autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
 });
 
 
+// Handling message to launch puppeteer from renderer)
+
+
+// notifications 
+
+const plat = 'linkedin'
+
+// const NOTIFICATION_TITLE = `We're launching a campaign on ${plat}`
+// const NOTIFICATION_BODY = 'Please open the browser for login'
+
+// const showNotification = () => {
+//   new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
+// }
+
+
 ipcMain.handle("get/puppeteer", async (event, args)=>{
-	console.log('listening to renderer from main');
+	console.log('get/puppeteer');
 	stealth()
   });
--- public/stealth.js
+++ public/stealth.js
@@ -1,36 +1,88 @@
 
 const puppeteer = require('puppeteer-extra');
 const hidden = require('puppeteer-extra-plugin-stealth')
-const { executablePath } = require('puppeteer')
+const { executablePath } = require('puppeteer');
+const fs = require('fs/promises');
+const { Notification} = require("electron");
 
 
 module.exports= stealth = () =>  (async () => {
 // Launch sequence
-    
+const NOTIFICATION_TITLE = `We're launching a campaign on linkedin`
+const NOTIFICATION_BODY = 'Please open the browser for login'
+
+const showNotification = () => {
+  new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
+}
+
+   
 puppeteer.use(hidden())
 
   const browser = await puppeteer.launch({
-    args: [
-      '--no-sandbox',
-    ],
+    args: ['--no-sandbox',],
     headless: false,
     ignoreHTTPSErrors: true,
-    //add this
     executablePath: executablePath(),
+    devtools: true,
   })
+
+  await showNotification() ;
   
   const page = await browser.newPage()
   await page.setViewport({
-    width: 1920,
-    height: 1280,
+    width: 1200,
+    height: 650,
     deviceScaleFactor: 1,
   });
 
 
 //Go to page
 
-  await page.goto('https://google.com/', {
+  await page.goto('https://www.linkedin.com/', {
     waitUntil: 'networkidle0',
   });
 
+  const cookiesString = await fs.readFile('./public/cookies.js');
+  const cookiez = JSON.parse(cookiesString);
+  await page.setCookie(...cookiez);
+  await console.log('setting cookies')
+
+  const sleep = (milliseconds) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  };
+  
+  await sleep(5000);
+
+  await page.reload();
+  
+  //  const cookies = await page.cookies();
+  //  await fs.writeFile('./public/cookies.js', JSON.stringify(cookies,null,2))
+  //  await console.log('saving cookies');
+
 })();
+
+
+/* Sequence
+Platform, Job Title, Location 
+
+switch platform  
+  case linkedin: path = htttps://www.linkedin.com/{jobtitle}/location }
+  case glassdoor: path =   
+  
+
+browser.newPage({path})
+
+*/
+
+/* 
+What can I do to save login cookies 
+Wait for 60 sec
+*/
+
+
+/*
+cookiesLinkedin
+cookiesGlassdoor
+cookiesIndeed
+*/
+
